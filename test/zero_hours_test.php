1af4f2786dc46bc15faf6916f6d091c57e31f306
Valerie-18 <72465385+Valerie-18@users.noreply.github.com>
2020-12-19 22:54:40 -0800

    Create zero_hours_test.php

---
+++ test/zero_hours_test.php
@@ -0,0 +1,24 @@
+<?php
+//zero_hours_test.php
+include_once 'includes/settings.php';
+require_once 'simpletest/autorun.php';
+require_once 'simpletest/web_tester.php';
+
+class ZeroHoursForm extends WebTestCase {
+
+      function testIncorrectData() {
+        $this->get(VIRTUAL_PATH . '/hours.php');
+        $this->assertResponse(200);
+
+                $this->setField("hours", "0");
+        $this->setField("rate", "0");
+        $this->clickSubmit("Show Pay");
+
+        $this->assertResponse(200);
+        $this->assertText("You input 0 hours at a rate of $0 and your pay is $0");
+    }
+
+
+
+
+}
